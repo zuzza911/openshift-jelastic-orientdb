4570915bdfc29a57952eb9ad798012ccfa24fac6
jelastic-public-cartridges <jelastic-public-cartridges@hivext.net>
2014-07-04 17:42:41 +0300

    fx

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -12,6 +12,6 @@ ORIENTDB_RUN_SCRIPT="${OPENSHIFT_ORIENTDB_DIR}/versions/1.7.4/bin/server.sh";
  memory_total=`free -m | grep Mem | awk '{print $2}'`;
  [ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}m";  }
 
-$SED -i "s/-Xmx=\([0-9]*[mM]\)*/-Xmx=${XMX}/" $ORIENTDB_RUN_SCRIPT;
-$SED -i "s/-Xms=\([0-9]*[mM]\)*/-Xms=${XMS}/" $ORIENTDB_RUN_SCRIPT;
+$SED -i "s/-Xmx\([0-9]*[mM]\)*/-Xmx${XMX}/" $ORIENTDB_RUN_SCRIPT;
+$SED -i "s/-Xms\([0-9]*[mM]\)*/-Xms${XMS}/" $ORIENTDB_RUN_SCRIPT;
 
