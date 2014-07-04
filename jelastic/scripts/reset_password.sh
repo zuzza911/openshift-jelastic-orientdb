5beb775c8e5007e92991556c6114f4dd9fbe98e8
Firstname Lastname <your_email@youremail.com>
2014-07-04 16:27:05 +0300

    uypd

--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -12,10 +12,8 @@ J_OPENSHIFT_APP_ADM_USER="root";
 
 function _setPassword() {
     ORIENTDB_SERVER_PROP_FILE="${OPENSHIFT_ORIENTDB_DIR}/versions/1.7.4/config/orientdb-server-config.xml";
-    
+    service cartridge stop;  
     $SED -i "s/password.* resources/password=\"${J_OPENSHIFT_APP_ADM_PASSWORD}\" resources/g" $ORIENTDB_SERVER_PROP_FILE;
-    service cartridge stop &
-    sleep 5s && pkill java;
     service cartridge start;
 }
 
