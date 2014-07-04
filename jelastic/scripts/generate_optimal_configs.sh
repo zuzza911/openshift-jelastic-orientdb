8990b806571627f8a43d7e3cc92341ae7e749634
Firstname Lastname <your_email@youremail.com>
2014-07-04 11:59:31 +0300

    scripts updated

--- jelastic/scripts/generate_optimal_configs.sh
+++ jelastic/scripts/generate_optimal_configs.sh
@@ -6,15 +6,12 @@ SED=$(which sed);
 # config optimizer for neo4j
 #
 
-#NEO4J_WRAPPER_PROP_FILE="${OPENSHIFT_NEO4J_DIR}/versions/1.9/neo4j-1.9/conf/neo4j-wrapper.conf";
+ORIENTDB_RUN_SCRIPT="${OPENSHIFT_ORIENTDB_DIR}/versions/1.7.4/bin/server.sh";
 
- [ -z "$XMS" ] && { XMS=32; }
+ [ -z "$XMS" ] && { XMS=32m; }
  memory_total=`free -m | grep Mem | awk '{print $2}'`;
- [ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}";  }
+ [ -z "$XMX" ] && { let XMX=memory_total-35; XMX="${XMX}m";  }
 
-#$SED -i "s/^#wrapper.java.initmemory/wrapper.java.initmemory/g";
-#$SED -i "s/^wrapper.java.initmemory=.*/wrapper.java.initmemory=${XMS}/" $NEO4J_WRAPPER_PROP_FILE;
-
-#$SED -i "s/^#wrapper.java.maxmemory/wrapper.java.maxmemory/g";
-#$SED -i "s/^wrapper.java.maxmemory=.*/wrapper.java.maxmemory=${XMX}/" $NEO4J_WRAPPER_PROP_FILE;
+$SED -i "s/-Xmx=\([0-9]*[mM]\)*/-Xmx=${XMX}/" $ORIENTDB_RUN_SCRIPT;
+$SED -i "s/-Xms=\([0-9]*[mM]\)*/-Xms=${XMS}/" $ORIENTDB_RUN_SCRIPT;
 
--- jelastic/scripts/reset_password.sh
+++ jelastic/scripts/reset_password.sh
@@ -8,14 +8,14 @@ SED=$(which sed);
 
 #$J_OPENSHIFT_APP_ADM_USER        ;   Operate this variable for the username
 #$J_OPENSHIFT_APP_ADM_PASSWORD    ;   Use this varible for your password
+J_OPENSHIFT_APP_ADM_USER="root";
 
 function _setPassword() {
-    return 0;
-    NEO4J_SERVER_PROP_FILE="${OPENSHIFT_NEO4J_DIR}/versions/1.9/neo4j-1.9/conf/neo4j-server.properties";
-    $SED -i "s/^org.neo4j.server.credentials=.*/org.neo4j.server.credentials=${J_OPENSHIFT_APP_ADM_USER}:${J_OPENSHIFT_APP_ADM_PASSWORD}/" $NEO4J_SERVER_PROP_FILE;
-    service cartridge restart;
+    ORIENTDB_SERVER_PROP_FILE="${OPENSHIFT_ORIENTDB_DIR}/versions/1.7.4/config/orientdb-server-config.xml";
+    service cartridge stop;
+    $SED -i "s/password.* resources/password=\"${J_OPENSHIFT_APP_ADM_PASSWORD}\" resources/g" $ORIENTDB_SERVER_PROP_FILE;
+    service cartridge start;
 }
-
 
 
 
